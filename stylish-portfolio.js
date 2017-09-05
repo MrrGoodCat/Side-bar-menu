5b24297b72e31c8a18c656d6c5f9009dad29d2f4
MrrGoodCat <mrrgoodcat@gmail.com>
2017-09-06 00:17:18 +0300

    Rename Classes

--- stylish-portfolio.js
+++ stylish-portfolio.js
@@ -4,32 +4,32 @@ var isMenuOpen = false;
   "use strict"; // Start of use strict
 
   // Opens the sidebar menu
-  $("#menu-toggle").click(function(e) {
+  $("#menu-container-toggle").click(function(e) {
     e.preventDefault();
       if(isMenuOpen)
           {
-            $("#menu-toggle-small").removeClass("active", 50);
+            $("#menu-container").removeClass("active", 50);
             isMenuOpen = false;
           }
       else if(!isMenuOpen)
           {
-            $("#menu-toggle-small").addClass("active", 50);
+            $("#menu-container").addClass("active", 50);
             isMenuOpen = true;  
           }
   });
 
   // Expands the sidebar menu
-  $("#sidebar-small").hover(function(e) {     
+  $("#sidebar-menu").hover(function(e) {     
     e.preventDefault();
       if(!isMenuOpen)
           {
-            $(".menu-child").toggleClass("toggle");
+            $(".menu-child").toggleClass("hover-efect");
           }
   });
     
     $(".menu-child").click(function (e) {
         e.preventDefault();
-        $("#menu-toggle-small").removeClass("active", 50);
+        $("#menu-container").removeClass("active", 50);
         isMenuOpen = false;
     });
 })(jQuery); // End of use strict
